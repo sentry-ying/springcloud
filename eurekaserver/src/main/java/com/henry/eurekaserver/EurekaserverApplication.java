01a5e24fa058fd04a12b0956c692d61823c95715
ying.senjun@rongzer.com <ying.senjun@rongzer.com>
2020-07-21 15:51:12 +0800

    创建项目并提交

---
+++ eurekaserver/src/main/java/com/henry/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,18 @@
+package com.henry.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka作为注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaserverApplication.class, args);
+    }
+
+}
